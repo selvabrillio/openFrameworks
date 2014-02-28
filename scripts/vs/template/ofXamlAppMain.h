7ab08735720d3c3b493048bcb8884acb3ade8953
steven-chith <steven.chith@gmail.com>
2014-02-27 19:33:12 -0800

    Added missing XAML template files

---
+++ scripts/vs/template/ofXamlAppMain.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "AngleBase.h"
+
+// Renders Direct2D and 3D content on the screen.
+namespace ofXamlApp
+{
+	ref class ofXamlAppMain sealed : public AngleBase
+	{
+	public:
+		ofXamlAppMain();
+
+		// AngleBase methods.
+		// Method for updating time-dependent objects.
+		void Update();
+		virtual void OnOrientationChanged() override;
+
+	protected:
+		virtual void OnRender() override;
+		virtual void CreateGLResources() override;
+	};
+}
