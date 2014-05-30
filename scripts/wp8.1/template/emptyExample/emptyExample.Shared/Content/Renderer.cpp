4a9713aeb550d1901311ef2f203556d8e3988a8d
Dale Stammen <dalestam@microsoft.com>
2014-05-29 20:35:11 -0700

    refactor Renderer

---
+++ scripts/wp8.1/template/emptyExample/emptyExample.Shared/Content/Renderer.cpp
@@ -0,0 +1,100 @@
+﻿#include "pch.h"
+#include "Renderer.h"
+
+#include "..\Common\DirectXHelper.h"
+
+using namespace emptyExample;
+
+using namespace DirectX;
+using namespace Windows::Foundation;
+
+// Loads vertex and pixel shaders from files and instantiates the cube geometry.
+Renderer::Renderer(const std::shared_ptr<DX::DeviceResources>& deviceResources) :
+	m_loadingComplete(false),
+	m_tracking(false),
+	m_deviceResources(deviceResources)
+{
+	CreateDeviceDependentResources();
+	CreateWindowSizeDependentResources();
+}
+
+// Initializes view parameters when the window size changes.
+void Renderer::CreateWindowSizeDependentResources()
+{
+	Size outputSize = m_deviceResources->GetOutputSize();
+	float aspectRatio = outputSize.Width / outputSize.Height;
+	float fovAngleY = 70.0f * XM_PI / 180.0f;
+
+	// This is a simple example of change that can be made when the app is in
+	// portrait or snapped view.
+	if (aspectRatio < 1.0f)
+	{
+		fovAngleY *= 2.0f;
+	}
+
+	// Note that the OrientationTransform3D matrix is post-multiplied here
+	// in order to correctly orient the scene to match the display orientation.
+	// This post-multiplication step is required for any draw calls that are
+	// made to the swap chain render target. For draw calls to other targets,
+	// this transform should not be applied.
+
+	// This sample makes use of a right-handed coordinate system using row-major matrices.
+	XMMATRIX perspectiveMatrix = XMMatrixPerspectiveFovRH(
+		fovAngleY,
+		aspectRatio,
+		0.01f,
+		100.0f
+		);
+
+	XMFLOAT4X4 orientation = m_deviceResources->GetOrientationTransform3D();
+}
+
+// Called once per frame, rotates the cube and calculates the model and view matrices.
+void Renderer::Update(DX::StepTimer const& timer)
+{
+	if (!m_tracking)
+	{
+
+	}
+}
+
+
+void Renderer::StartTracking()
+{
+	m_tracking = true;
+}
+
+// When tracking, the 3D cube can be rotated around its Y axis by tracking pointer position relative to the output screen width.
+void Renderer::TrackingUpdate(float positionX)
+{
+	if (m_tracking)
+	{
+	}
+}
+
+void Renderer::StopTracking()
+{
+	m_tracking = false;
+}
+
+// Renders one frame using the vertex and pixel shaders.
+void Renderer::Render()
+{
+	// Loading is asynchronous. Only draw geometry after it's loaded.
+	if (!m_loadingComplete)
+	{
+		return;
+	}
+
+	auto context = m_deviceResources->GetD3DDeviceContext();
+}
+
+void Renderer::CreateDeviceDependentResources()
+{
+
+}
+
+void Renderer::ReleaseDeviceDependentResources()
+{
+	m_loadingComplete = false;
+}
