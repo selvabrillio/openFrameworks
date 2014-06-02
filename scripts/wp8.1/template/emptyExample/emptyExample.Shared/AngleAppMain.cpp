ca62b81d66ba87a3ed42cd53de86fc174ad415cf
Dale Stammen <dalestam@microsoft.com>
2014-06-01 19:06:03 -0700

    added input event queue

---
+++ scripts/wp8.1/template/emptyExample/emptyExample.Shared/AngleAppMain.cpp
@@ -0,0 +1,143 @@
+﻿#include "pch.h"
+#include "AngleAppMain.h"
+#include "Common\AngleHelper.h"
+
+using namespace Windows::Foundation;
+using namespace Windows::System::Threading;
+using namespace Windows::UI::Core;
+using namespace Concurrency;
+
+namespace AngleApp
+{
+
+// Loads and initializes application assets when the application is loaded.
+AngleAppMain::AngleAppMain(const std::shared_ptr<AngleApp::DeviceResources>& deviceResources) 
+    : m_deviceResources(deviceResources)
+    , m_tracking(false)
+{
+	// Register to be notified if the Device is lost or recreated
+	m_deviceResources->RegisterDeviceNotify(this);
+
+	// TODO: Replace this with your app's content initialization.
+	m_renderer = std::unique_ptr<Renderer>(new Renderer(m_deviceResources));
+
+	// TODO: Change the timer settings if you want something other than the default variable timestep mode.
+	// e.g. for 60 FPS fixed timestep update logic, call:
+	/*
+	m_timer.SetFixedTimeStep(true);
+	m_timer.SetTargetElapsedSeconds(1.0 / 60);
+	*/
+}
+
+AngleAppMain::~AngleAppMain()
+{
+	// Deregister device notification
+	m_deviceResources->RegisterDeviceNotify(nullptr);
+}
+
+// Updates application state when the window size changes (e.g. device orientation change)
+void AngleAppMain::CreateWindowSizeDependentResources() 
+{
+	// TODO: Replace this with the size-dependent initialization of your app's content.
+	m_renderer->CreateWindowSizeDependentResources();
+}
+
+void AngleAppMain::StartRenderLoop()
+{
+	// If the animation render loop is already running then do not start another thread.
+	if (m_renderLoopWorker != nullptr && m_renderLoopWorker->Status == AsyncStatus::Started)
+	{
+		return;
+	}
+
+	// Create a task that will be run on a background thread.
+	auto workItemHandler = ref new WorkItemHandler([this](IAsyncAction ^ action)
+	{
+		// Calculate the updated frame and render once per vertical blanking interval.
+		while (action->Status == AsyncStatus::Started)
+		{
+			critical_section::scoped_lock lock(m_criticalSection);
+			Update();
+            Render();
+
+		}
+	});
+
+	// Run task on a dedicated high priority background thread.
+	m_renderLoopWorker = ThreadPool::RunAsync(workItemHandler, WorkItemPriority::High, WorkItemOptions::TimeSliced);
+}
+
+void AngleAppMain::StopRenderLoop()
+{
+	m_renderLoopWorker->Cancel();
+}
+
+// Updates the application state once per frame.
+void AngleAppMain::Update() 
+{
+	// Update scene objects.
+	m_timer.Tick([&]()
+	{
+		// TODO: Replace this with your app's content update functions.
+		m_renderer->Update(m_timer);
+	});
+}
+
+
+
+// Renders the current frame according to the current application state.
+// Returns true if the frame was rendered and is ready to be displayed.
+bool AngleAppMain::Render() 
+{
+	// Don't try to render anything before the first Update.
+	if (m_timer.GetFrameCount() == 0)
+	{
+		return false;
+	}
+
+	// Render the scene objects.
+	// TODO: Replace this with your app's content rendering functions.
+	m_renderer->Render();
+
+	return true;
+}
+
+// Notifies renderers that device resources need to be released.
+void AngleAppMain::OnDeviceLost()
+{
+	m_renderer->ReleaseDeviceDependentResources();
+}
+
+// Notifies renderers that device resources may now be recreated.
+void AngleAppMain::OnDeviceRestored()
+{
+	//m_renderer->CreateDeviceDependentResources();
+	//CreateWindowSizeDependentResources();
+}
+
+
+void AngleAppMain::OnPointerPressed(PointerEventArgs^ e)
+{
+    m_tracking = true;
+    m_renderer->AddPointerEvent(PointerEventType::PointerPressed, e);
+}
+
+void AngleAppMain::OnPointerMoved(PointerEventArgs^ e)
+{
+    if (IsTracking())
+    {
+        m_renderer->AddPointerEvent(PointerEventType::PointerMoved, e);
+    }
+}
+
+void AngleAppMain::OnPointerReleased(PointerEventArgs^ e)
+{
+    m_tracking = false;
+    m_renderer->AddPointerEvent(PointerEventType::PointerReleased, e);
+}
+
+
+
+}
+
+
---
+++ scripts/wp8.1/template/emptyExample/emptyExample.Shared/Common/Renderer.cpp
@@ -0,0 +1,120 @@
+﻿#include "pch.h"
+#include "Renderer.h"
+#include "ofAppRunner.h"
+#include "ofAppWinRTWindow.h"
+
+#include "..\Common\AngleHelper.h"
+
+
+using namespace DirectX;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Core;
+
+extern int ofmain();
+
+namespace AngleApp
+{
+
+
+// Loads vertex and pixel shaders from files and instantiates the cube geometry.
+Renderer::Renderer(const std::shared_ptr<AngleApp::DeviceResources>& deviceResources) :
+	m_loadingComplete(false),
+	m_deviceResources(deviceResources)
+{
+    CreateDeviceDependentResources();
+}
+
+// Initializes view parameters when the window size changes.
+void Renderer::CreateWindowSizeDependentResources()
+{
+    if (m_loadingComplete)
+    {
+        Size outputSize = m_deviceResources->GetOutputSize();
+        Size logicalSize = m_deviceResources->GetLogicalSize();
+        ofAppWinRTWindow* window = reinterpret_cast<ofAppWinRTWindow*>(ofGetWindowPtr());
+        window->OnWindowSizeChanged(logicalSize.Width, logicalSize.Height);
+    }
+}
+
+// Called once per frame, rotates the cube and calculates the model and view matrices.
+void Renderer::Update(AngleApp::StepTimer const& timer)
+{
+
+}
+
+void Renderer::OnPointerPressed(PointerEventArgs^ e)
+{
+    ofAppWinRTWindow* window = reinterpret_cast<ofAppWinRTWindow*>(ofGetWindowPtr());
+    window->OnPointerPressed(e);
+}
+
+void Renderer::OnPointerMoved(PointerEventArgs^ e)
+{
+    ofAppWinRTWindow* window = reinterpret_cast<ofAppWinRTWindow*>(ofGetWindowPtr());
+    window->OnPointerMoved(e);
+}
+
+void Renderer::OnPointerReleased(PointerEventArgs^ e)
+{
+    ofAppWinRTWindow* window = reinterpret_cast<ofAppWinRTWindow*>(ofGetWindowPtr());
+    window->OnPointerReleased(e);
+}
+
+void Renderer::AddPointerEvent(PointerEventType type, PointerEventArgs^ args)
+{
+    std::lock_guard<std::mutex> guard(mMutex);
+    std::shared_ptr<PointerEvent> e(new PointerEvent(type, args));
+    mInputEvents.push(e);
+}
+
+void Renderer::ProcessEvents()
+{
+    std::lock_guard<std::mutex> guard(mMutex);
+
+    while (!mInputEvents.empty())
+    {
+        InputEvent* e = mInputEvents.front().get();
+        e->execute(this);
+        mInputEvents.pop();
+    }
+}
+
+
+// Renders one frame using the vertex and pixel shaders.
+void Renderer::Render()
+{
+	if (m_loadingComplete)
+	{
+        m_deviceResources->aquireContext();
+
+        ProcessEvents();
+#if 0
+        glClearColor(1.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+#else
+        ofAppWinRTWindow* window = reinterpret_cast<ofAppWinRTWindow*>(ofGetWindowPtr());
+        window->RunOnce();
+#endif
+        m_deviceResources->Present();
+
+        m_deviceResources->releaseContext();
+    }
+}
+
+void Renderer::CreateDeviceDependentResources()
+{
+    if (!m_loadingComplete)
+    {
+        m_deviceResources->aquireContext();
+        ofmain();
+        m_loadingComplete = true;
+        m_deviceResources->releaseContext();
+    }
+}
+
+void Renderer::ReleaseDeviceDependentResources()
+{
+	m_loadingComplete = false;
+}
+
+}
